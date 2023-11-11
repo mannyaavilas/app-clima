5a0ea27e1ca528671c93be6ae46bea148eb06427
mannyavila <mannyavila90@gmail.com>
2023-11-11 15:23:46 -0400

    Formulario creado

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 
 function Formulario() {
   return (
-    <div>
-      Formulario
+    <div className='contenedor'>
+      <form>
+        <div className="campo">
+            <label htmlFor="ciudad">Ciudad</label>
+            <input type="text"
+                id='ciudad'
+                name='pais'
+            />
+
+        </div>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <select 
+            name="pais" 
+            id="pais"
+          >
+            
+            <option value="">Seleccione un Pais</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">Mexico</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+          </select>
+        </div>
+
+        <input type="submit" value='Consultar Clima' />
+      </form>
     </div>
   )
 }
