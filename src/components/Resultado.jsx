a0a4d120935b3de3a9ae2d31d9e0c82342f9d3bd
mannyavila <mannyavila90@gmail.com>
2023-11-13 15:01:41 -0400

    Añadir el resto de la informacion del clima

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -4,11 +4,22 @@ const Resultado = () => {
 
     const {resultado} = useClima()
 
-    const {name} = resultado
+    const {name, main} = resultado
+
+    console.log(resultado)
+
+    // Grados kelvin
+    const kelvin = 273.15
 
   return (
-    <div className="contenedor">
-      <h2>El clima de {name} es:</h2>
+    <div className="contenedor clima">
+      <h2>El Clima de {name} es:</h2>
+
+      <p>{parseInt(main.temp - kelvin)} <span>&#x2103;</span></p>
+      <div className="min-max">
+        <p>Mín: {parseInt(main.temp_min - kelvin)} <span>&#x2103;</span></p>
+        <p>Máx: {parseInt(main.temp_max - kelvin)} <span>&#x2103;</span></p>
+      </div>
     </div>
   )
 }
